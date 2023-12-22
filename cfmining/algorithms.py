66193150ce488c109e8d818d788e81c60e596b41
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2023-12-22 16:55:32 -0300

    fix error on outlier detection

--- cfmining/algorithms.py
+++ cfmining/algorithms.py
@@ -733,7 +733,7 @@ class MAPOCAM2:
             if new_proba >= self.clf.threshold - self.eps:                
                 # Only save if it is not an outlier
                 if self.outlier_detection is not None:
-                    outlier = self.outlier_detection.predict(new_solution[None, :]) == 1
+                    outlier = self.outlier_detection.predict(new_solution[None, :]) == -1
 
                 if outlier:
                     self.outlier_detection_counter += 1
