93b250a79f4b619cc353bfe742582592d72c4e9b
Giovani Valdrighi <giovani.valdrighi@gmail.com>
2024-03-08 14:35:22 -0300

    starting created metrics for counterfactuals

--- cfmining/utils.py
+++ cfmining/utils.py
@@ -10,3 +10,22 @@ class OutlierWrap:
         pred = self.outlier_clf.predict(X)
         pred = np.where(pred < self.threshold, 1, -1)
         return pred
+
+
+def diversity_metric(solutions):
+    """Measure the diversity metric of solutions."""
+    l1_dist_matrix = np.abs(solutions[:, None] - solutions[None, :])
+    print(l1_dist_matrix.shape)
+    l1_dist_matrix += np.eye(len(solutions)) * 1e-4
+    K = 1 / (1 + l1_dist_matrix)
+    return np.linalg.det(K)
+
+
+def proximity_metric(individual, solutions):
+    """Measure the proximity metric of solutions, i.e., the sum of L1 distances."""
+    return np.sum(np.abs(individual - solutions), axis=1)
+
+
+def sparsity_metric(individual, solutions):
+    """Measure the sparsity metric of solutions, i.e., the number of changes."""
+    return np.sum(individual != solutions, axis=1)
